89639239c2e73f98ca96a9ef4270cf52b535509a
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2022-10-12 16:45:54 +0200

    Added feature to detect and add ingredients

--- hand.py
+++ hand.py
@@ -1,3 +1,4 @@
+from math import prod
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
@@ -16,9 +17,10 @@ def get_product_catalog() :
 
 get_product_catalog()
 
-# Create csv file.
+# Create csv file with all product data.
 with open('database.csv', 'w') as csv_file :
-  columns = ['Title', 'Price root', 'Price promo']
+  # Set csv headings
+  columns = ['title', 'price_root', 'price_promo', 'content', 'ingredients',]
   csv_write = writer(csv_file)
   csv_write.writerow(columns)
 
@@ -32,16 +34,53 @@ with open('database.csv', 'w') as csv_file :
     target_selector_price_root = 'price-amount_root__37xv2 product-card-hero-price_now__PlF9u'
     target_selector_price_promo_now = 'price-amount_root__37xv2 price-amount_bonus__27nxZ product-card-hero-price_now__PlF9u'
     target_selector_price_promo_prev = 'price-amount_root__37xv2 price-amount_was__1PrUY product-card-hero-price_was__1ZNtq'
+    target_selector_content = 'product-card-header_unitInfo__2ncbP'
+    target_selector_ingredients = 'typography_root__18FkK typography_variant-paragraph__33rgM typography_hasMargin__26L1z'
 
+    # Get data from the product link
+    # Title
     title = soup.find(class_=target_selector_title)
     title = title.get_text().strip()
+
+    # Price
     price_now = soup.find(class_=target_selector_price_root)
     price_prev = 'null'
+    # If root price is not detected, check for promo price
     if price_now == None :
       price_now = soup.find(class_=target_selector_price_promo_now)
       price_prev = soup.find(class_=target_selector_price_promo_prev)
     price_now = price_now.get_text().strip()
     if price_prev != 'null' :
       price_prev = price_prev.get_text().strip()
-    print([title, price_now, price_prev])
-    csv_write.writerow([title, price_now, price_prev])
+
+    # Content
+    content = soup.find(class_=target_selector_content)
+    span = content.span
+    if span != None :
+      content.span.decompose()
+    content = content.get_text().strip()
+    
+    # Ingredients
+    content_blocks = soup.find_all(class_='product-info-content-block')
+    for block in content_blocks :
+      ingredients = block.find('h2')
+      if ingredients == None :
+        continue
+      else :
+        ingredients = ingredients.get_text().strip()
+        if ingredients == 'Ingrediënten' :
+          ingredients = block.find(class_=target_selector_ingredients)
+          if ingredients == None :
+            ingredients = 'null'
+          else :
+            ingredients = ingredients.get_text().strip()
+            ingredients = ingredients.replace(u'\xa0', u' ')
+            break
+        else :
+          continue
+    
+    # Print link data into csv file
+    product = [title, price_now, price_prev, content, ingredients,]
+    print(product)
+    print('')
+    csv_write.writerow(product)
